ea7624b38e5daa697149d9167fb79e84f875383b
UDAY2003 <84787861+UDAY2003@users.noreply.github.com>
2021-08-26 12:41:05 +0530

    Done

--- Program-3.c
+++ Program-3.c
@@ -11,7 +11,7 @@
 //The concatenated  string = Nithinkumar
 
 #include <stdio.h>
-#include<string.h>
+
 int main() {
 	char a[20], b[20], c[20];
 	int i, j;
@@ -19,16 +19,20 @@ int main() {
 	scanf("%s", a);
 	printf("Enter the second string : ");
 	scanf("%s", b);
-	for (i=0;i<strlen(a);i++)             // Complete the code in for
+	for (i=0;;i++)             // Complete the code in for
            { 
+		if(a[i]=='\0')
+		break;
 		c[i] =a[i] ;                                              //Complete the statement
 	}
-	for (j=0;j<strlen(b);j++ )                                                 // Complete the code in for
+	for (j=0;;j++ )                                                 // Complete the code in for
            { 
+		if (b[j]=='\0')
+		break;	
 		c[i] = b[j];                                              //Complete the statement
 		i++;
 	}
-	c[i]= b[j];                                               //Complete the statement
+	c[i]= '\0';                                               //Complete the statement
 	printf("The concatenated string = %s", c);
 	return 0;
 }
