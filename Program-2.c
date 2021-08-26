a2c3c936c0b5c8a0c82c39bbf6bfded07a3ea9af
UDAY2003 <84787861+UDAY2003@users.noreply.github.com>
2021-08-26 12:27:24 +0530

    Done

--- Program-2.c
+++ Program-2.c
@@ -9,15 +9,15 @@
 //The copied String-2 is Nithin
 
 #include <stdio.h>
-#include<string.h>
 int main() 
 {
 	char str1[50], str2[50];
 	int i;
-	// printf("Enter a string-1: ");
+	 printf("Enter a string-1: ");
 	scanf("%s",str1);
-	for (i=0;str1[i]!='\0';i++)                  //Complete the code in for-loop & copy logic
-  {                          
+	for (i=0;;i++)                  //Complete the code in for-loop & copy logic
+	{  if(str1[i]=='\0')
+	        break;
 		str2[i]=str1[i] ;
 	}
 	str2[i]= '\0';                  //Complete the statement
